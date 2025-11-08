4a151f8eb574f42062f51ee01ff82bfdb0726d4f
smaurya4941 <smaurya4941@gmail.com>
2025-11-08 19:54:18 +0530

    vercel

--- LifeLink/lifelink_frontend/vite.config.js
+++ LifeLink/lifelink_frontend/vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    port: 5173
+  }
+})
