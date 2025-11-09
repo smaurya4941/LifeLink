0e4d161637ea238afb1c20f6c1d74f3b4961fe5f
smaurya4941 <smaurya4941@gmail.com>
2025-11-09 06:30:32 +0530

    fixing issue

--- LifeLink/lifelink_frontend/vite.config.js
+++ LifeLink/lifelink_frontend/vite.config.js
@@ -1,7 +1,7 @@
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',       // ✅ Add this line
+    outDir: 'dist',  // ✅ Add this to ensure Vercel finds your build
     chunkSizeWarningLimit: 1000
   },
   server: {
